98101e9136f50670088fc1289952fed91e5105f8
parkhoryeon <hrpark930908@naver.com>
2023-11-10 17:27:05 +0900

    Commit
    231110_1727

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/client/js/main.js",
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "assets", "js"),
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ],
+                // plugins: ['@babel/plugin-proposal-class-properties']
+              }
+            }
+          }
+        ]
+      }
+    }
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: "babel-loader",
+//                     options: {
+//                         presets: [["@babel/preset-env", { targets: "defaults" }]],
+//                     }
+//                 }
+//             }
+//         ]
+//     }
+// }
